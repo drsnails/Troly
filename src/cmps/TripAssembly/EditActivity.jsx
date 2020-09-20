70f47c8ad7089d07460217af285ecf52ce1092a3
Stav Yaar Bar <anistu@gmail.com>
2020-09-20 12:43:03 +0300

    asd

--- src/cmps/TripAssembly/EditActivity.jsx
+++ src/cmps/TripAssembly/EditActivity.jsx
@@ -10,6 +10,7 @@ export class EditActivity extends Component {
             duration: '',
             notes: '',
             price: '',
+            // destination: '',
             id: null
         }
 
